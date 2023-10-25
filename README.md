e93271a5a9840b08a698dbfe9a8407e34074500f
Jacob Harwood <114497950+jacobharwood7@users.noreply.github.com>
2023-10-25 20:59:50 +0100

    intial

---
+++ README.md
@@ -0,0 +1,2 @@
+# C4E_Project
+ 
---
+++ Source/BeanSpells/BeanSpellsGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "BeanSpellsGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BEANSPELLS_API ABeanSpellsGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
