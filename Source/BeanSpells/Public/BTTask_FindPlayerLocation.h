7d2d7df3417ab07199a9a9d7cdec2c240a8df16b
Jacob Harwood <114497950+jacobharwood7@users.noreply.github.com>
2023-11-03 21:13:15 +0000

    findplayer

---
+++ Source/BeanSpells/Public/BTTask_FindPlayerLocation.h
@@ -0,0 +1,28 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/Tasks/BTTask_BlackboardBase.h"
+#include "BTTask_FindPlayerLocation.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BEANSPELLS_API UBTTask_FindPlayerLocation : public UBTTask_BlackboardBase
+{
+public:
+	explicit UBTTask_FindPlayerLocation(FObjectInitializer const& ObjectInit);
+	virtual EBTNodeResult::Type ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) override;
+
+	
+private:
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere,BlueprintReadWrite, Category = "Search", meta=(AllowPrivateAccess="true"))
+	bool _searchRandom = false;
+
+	UPROPERTY(EditAnywhere,BlueprintReadWrite,Category="Search",meta=(AllowPrivateAccess="true"))
+	float _searchRadius = 150.0f;
+};
